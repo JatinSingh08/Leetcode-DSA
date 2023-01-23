aa366611725e5e0cb41c15b4595ba280fe51eb6a
Jatin Singh <97881261+JatinSingh08@users.noreply.github.com>
2023-01-23 19:32:45 +0530

    Time: 202 ms (70.85%), Space: 69 MB (21.25%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(trust.size() == 0 && n==1) return 1;
-        vector<int> outdegree(n+1,0);
-        vector<int> indegree(n+1,0);
-        for(auto t : trust) {
-            outdegree[t[0]]++;
-            indegree[t[1]]++;
+        
+        vector<int> out(n+1,0);
+        vector<int> in(n+1,0);
+        
+        for(auto t: trust) {
+            out[t[0]]++;
+            in[t[1]]++;
         }
         for(int i=0;i<=n;i++) {
-            if(outdegree[i] == 0 && indegree[i]==n-1) return i;
+            if(out[i] == 0 && in[i]==n-1) return i;
         }
         return -1;
     }
