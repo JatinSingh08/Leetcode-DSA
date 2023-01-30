84a95be64123e34070a27f689409243ed08d40b4
Jatin Singh <97881261+JatinSingh08@users.noreply.github.com>
2023-01-30 19:51:54 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (37.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        vector<int> t(n+1,0);
-        t[0] = 0;
-        t[1] = 1;
-        t[2] = 1;
-        
+        vector<int> temp = {0,1,1};
+        int var = 0;
         for(int i=3;i<=n;i++) {
-            t[i] = t[i-1]+t[i-2]+t[i-3];
+            var = temp[i-1]+temp[i-2]+temp[i-3];
+            temp.push_back(var);
         }
-        return t[n];
+        return temp[n];
     }
 };
